0e5180dcc3f8eb5f5c1f42ef7604a2cb7837755f
Darshan <darshanvkalola@gmail.com>
2023-11-01 13:23:39 -0400

    Add requirements

--- backend/sliq_project/settings.py
+++ backend/sliq_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "sliq_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",  # This is where you put the name of the db file.
+        "NAME": "database",  # This is where you put the name of the db file.
         # If one doesn't exist, it will be created at migration time.
     }
 }
