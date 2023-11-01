770faafe9dd8b7041aea67407f50f99d3eb9fe3f
Darshan <darshanvkalola@gmail.com>
2023-11-01 16:38:54 -0400

    Add npm install

--- run_locally.sh
+++ run_locally.sh
@@ -2,4 +2,4 @@ python backend/manage.py makemigrations
 python backend/manage.py migrate
 pip install -r requirements.txt
 nohup python backend/manage.py runserver >/dev/null 2>&1 &
-cd frontend && npm run dev
+cd frontend && npm install && npm run dev
